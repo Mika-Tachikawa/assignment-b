7bcae823cfa6f56937019df1a3413c2ead491e37
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-05-19 05:03:25 +0000

    Initial Commit1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << "21908c58cc3d407c8fe76587ff4deee5.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.active_job.queue_adapter = :inline
+
 end
