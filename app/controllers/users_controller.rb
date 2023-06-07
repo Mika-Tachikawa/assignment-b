6afb4be06edd2064b61ad025b0969c26c581fbf8
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-06-07 04:11:52 +0000

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,15 @@ class UsersController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(current_user.id)
+    else
+      render :users
+    end
+  end
+
   def index
     @users = User.all
     @book = Book.new
